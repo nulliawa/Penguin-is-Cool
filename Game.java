ad963b6ed8c034f43ac70e9de338a3163a671438
nulli <31201722jh@gmail.com>
2024-01-11 13:59:36 -0500

    Fix menu
    Provided button functionality

--- Game.java
+++ Game.java
@@ -3,18 +3,21 @@ import com.sun.source.doctree.AttributeTree;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Game extends BaseFrame {
-//    private final int WIDTH = 1920, HEIGHT = 1080;
-    private final int WIDTH=1366,HEIGHT=768;
-    public final int MENU = 0, GAME = 1;
+    //    private final int WIDTH = 1920, HEIGHT = 1080;
+    private final int WIDTH = 1366, HEIGHT = 768;
+    public final int MENU = 0, GAME = 1, TUTORIAL = 2;
     int screen = MENU; // Change to MENU when done gameplay
     private Player player;
     private BKG bkg;
+<<<<<<< Updated upstream
     private boolean sideMoveX=true,sideMoveY=true;
+=======
+    private boolean sideMove = false;
+>>>>>>> Stashed changes
     private int offsetDistance = 300; // How far from the edge of the offset
     // private ArrayList<Button> menuButtons = new ArrayList<>();
 
@@ -22,6 +25,7 @@ public class Game extends BaseFrame {
         super(title, width, height);
 
         // bkg = new BKG(width / 2, height / 2);
+<<<<<<< Updated upstream
         bkg = new BKG(0,0,1500, 1000);//wip
         bkg.setup();
         player = new Player(width/2, height/2);
@@ -36,12 +40,30 @@ public class Game extends BaseFrame {
 //    }
     public void move() {
         player.collision(new Rectangle());//WIP
+=======
+        bkg = new BKG(0, 0, 1500, 1000);//wip
+        bkg.setUp();
+        player = new Player(width / 2, height / 2);
+        player.setX(width / 2 - player.getRect().width / 2);
+        player.setY(height / 2 - player.getRect().height / 2);
+    }
+
+    private void pFixX() {
+        player.setX(WIDTH / 2 - player.getRect().width / 2);
+    }
+
+    private void pFixY() {
+        player.setY(HEIGHT / 2 - player.getRect().height / 2);
+    }
+
+    public void move(KeyEvent e) {
+>>>>>>> Stashed changes
         // If player gets to the edge of the background, stop moving the background, instead move the player
-        int bkgX=bkg.getOffX();
-        int bkgY=bkg.getOffY();
-        int pX=player.getRect().x;
-        int pY=player.getRect().y;
-        int[] coord= new int[]{bkgX, bkgY, pX, pY};
+        int bkgX = bkg.getOffX();
+        int bkgY = bkg.getOffY();
+        int pX = player.getRect().x;
+        int pY = player.getRect().y;
+        int[] coord = new int[]{bkgX, bkgY, pX, pY};
 
         System.out.println(Arrays.toString(coord));
 //        System.out.print(bkg.edgeX());
@@ -51,6 +73,7 @@ public class Game extends BaseFrame {
 //        System.out.println(sideMoveY);
 //        System.out.print(bkg.edgeY());
 //        System.out.print(player.pushY());
+<<<<<<< Updated upstream
 //        System.out.print(" ");
 //        System.out.print(player.isFixed());
 //        System.out.println();
@@ -81,6 +104,26 @@ public class Game extends BaseFrame {
         }
         if(player.isFixedY()){
             bkg.move(keys,true);
+=======
+        System.out.print(" ");
+        System.out.print(player.isFixed());
+        System.out.println();
+
+        if (player.isFixedX()) {
+            //player is fixed to middle line so not at an edge, can move background left/right
+            bkg.move(e.getKeyCode(), false);
+        }
+        if (player.isFixedY()) {
+            bkg.move(e.getKeyCode(), true);
+        }
+
+        if (bkg.edgeX()) {//arrive at edge of background left/right direction
+            player.move(e.getKeyCode(), false);
+            //player itself can move left/right and background stops left/right movement
+        }
+        if (bkg.edgeY()) {
+            player.move(e.getKeyCode(), true);
+>>>>>>> Stashed changes
         }
 
 //        else{
@@ -134,47 +177,49 @@ public class Game extends BaseFrame {
     public void drawMenu(Graphics g) {
         final int X = 0, Y = 1, W = 2, H = 3; // Variables for button creation
 
+        // Background screen
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, WIDTH, HEIGHT);
 
+        // Title
         g.setFont(new Font("SnowtopCaps", Font.PLAIN, 150));
         g.setColor(Color.CYAN);
-        g.drawString("Penguin is Cool",  WIDTH/2 - 550, 200);
+        g.drawString("Penguin is Cool", WIDTH / 2 - 550, 200);
 
         // Arrays for buttons
-        int[][] buttonCoordinates = {{WIDTH/2 - 200, 275, 400, 150}, {WIDTH/2 - 200, 525, 400, 150}};
+        int[][] buttonCoordinates = {{WIDTH / 2 - 200, 275, 400, 150}, {WIDTH / 2 - 200, 525, 400, 150}};
         String[] buttonText = {"Play", "Tutorial"};
         int[][] textCoordinates = {{640, 365}, {590, 620}};
-
-        // ArrayList<Button> buttons = new ArrayList<Button>();
+        ArrayList<Button> buttons = new ArrayList<>();
 
 
         // Button creation
         for (int i = 0; i < buttonCoordinates.length; i++) {
-            g.setColor(Color.BLUE); //TEMP COLOUR
-            Button temp = new Button(buttonCoordinates[i][X], buttonCoordinates[i][Y], buttonCoordinates[i][W], buttonCoordinates[i][H]); // Creates a new button object with params
+            g.setColor(Color.BLUE); // PLACEHOLDER
 
-            // buttons.add(temp);
-            g.fillRect(temp.getRect().x, temp.getRect().y, temp.getRect().width, temp.getRect().height); // Making new buttons
+            // Creating new button
+            Button temp = new Button(buttonCoordinates[i][X], buttonCoordinates[i][Y], buttonCoordinates[i][W], buttonCoordinates[i][H]);
+            buttons.add(temp);
 
+            g.fillRect(temp.getRect().x, temp.getRect().y, temp.getRect().width, temp.getRect().height); // Drawing buttons
+
+            // Drawing buttons when hovered
             if (temp.isHover(mx, my)) {
-                 // System.out.println("HELP");
-
-                g.setColor(Color.DARK_GRAY);
+                g.setColor(Color.DARK_GRAY); // PLACEHOLDER
                 g.fillRect(temp.getRect().x, temp.getRect().y, temp.getRect().width, temp.getRect().height);
-
-            }
-
-            if (temp.isClicked(mx, my, mb)) {
-                screen = GAME;
             }
 
             g.setFont(new Font("Comic Sans MS", Font.BOLD, 50));
             g.setColor(Color.WHITE);
-            g.drawString(buttonText[i], textCoordinates[i][0], textCoordinates[i][1]);
+            g.drawString(buttonText[i], textCoordinates[i][X], textCoordinates[i][Y]);
         }
 
-        // WIP: NEED TO DIFFERENTIATE CLICKS
+        // Button function
+        if (buttons.get(0).isClicked(mx, my, mb)) {
+            screen = GAME;
+        } else if (buttons.get(1).isClicked(mx, my, mb)) {
+            screen = TUTORIAL;
+        }
     }
 
     public void drawGame(Graphics g) {
@@ -183,12 +228,28 @@ public class Game extends BaseFrame {
         player.draw(g);
         g.setColor(Color.green);
     }
+<<<<<<< Updated upstream
     @Override
     public void draw(Graphics g) {
+=======
+
+    public void drawTutorial(Graphics g) {
+        // Background
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+        // Controls
+
+        // WHAT TO DO
+    }
+
+    public void draw(Graphics g) {//test
+>>>>>>> Stashed changes
         if (screen == MENU) {
             drawMenu(g);
         } else if (screen == GAME) {
             drawGame(g);
+        } else if (screen == TUTORIAL) {
+            drawTutorial(g);
         }
     }
 
