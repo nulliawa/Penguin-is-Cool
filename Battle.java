1d8ccc6d305cd56c6cfdb6dfcc83b8ca4a061b73
Ederexre <116308792+Ederexre@users.noreply.github.com>
2024-01-19 13:43:46 -0500

    cloud move

--- Battle.java
+++ Battle.java
@@ -10,7 +10,7 @@ public class Battle {
         private int x,y,spdX,spdY,width,height,spdScale;
         private int elli;
         private double heading;
-        public Projectile(int x,int y,int width,int height,double heading){
+        public Projectile(int x,int y,int width,int height,int spdScale,double heading){
             this.x=x;
             this.y=y;
             this.spdX=0;
@@ -27,12 +27,14 @@ public class Battle {
             this.width=width;
             this.height=height;
         }
-        public void move(){
+        public void move(boolean mv){
             //sideways movement
             this.spdX= (int) Math.round(spdScale*Math.cos(heading));
             this.spdY= -(int) Math.round(spdScale*Math.sin(heading));
-            this.x+=this.spdX;
-            this.y+=this.spdY;
+            if(mv) {
+                this.x += this.spdX;
+                this.y += this.spdY;
+            }
         }
         public boolean remove(ArrayList pList){//removes off-screen projectiles
             if(this.x+width<0){
@@ -62,18 +64,19 @@ public class Battle {
             }
         }
         public void draw(Graphics g){
-            g.setColor(Color.pink);
             g.fillRect(x,y,width,height);
         }
     }
     Random random=new Random();
-    private long memTime=0;
-    private final double PI=Math.PI;
-    private final int WIDTH = 1400, HEIGHT = 800,SIZE=30,RIGHT=0,LEFT=180,UP=90,DOWN=270;
+    private long[] memTime=new long[10];
+    private static final double PI=Math.PI;
+    private static final int WIDTH = 1400, HEIGHT = 800,SIZE=30,RIGHT=0,LEFT=180,UP=90,DOWN=270;
+    private static final int SNOWGEN=200,CLOUDSTALL=5000,ICESTALL=1000,GENERATE=2000;
     private BKG batBKG;
     private int hp;
     private ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
-    private ArrayList<Projectile> snow=new ArrayList<>();
+    private static ArrayList<Projectile> clouds=new ArrayList<>();
+    private static ArrayList<Projectile> snow=new ArrayList<>();
     private Player player=new Player(WIDTH/2,HEIGHT/2);
 
 //    Timer snowTimer = new Timer(200, new ActionListener() {
@@ -96,26 +99,28 @@ public class Battle {
     public int random(int low, int high){//easier to get random number
         return random.nextInt(low,high+1);
     }
-    public double rad(int degree){//direction in degrees
-        if(degree>360){
-            double angle=degree%360.0;
-            return angle/180.0*PI;
-        }
+    public double rad(int degree){//direction from degree to radians
         return degree/180.0*PI;
     }
-    private void createSnow(int x, int y){
-        snow.add(new Projectile(500+random(0,5)*10,y,10,10, rad(DOWN)));
+    //snow generates at a random x under a cloud
+    private void createSnow(Projectile cloud){
+        Rectangle cRect=cloud.getRect();
+        snow.add(new Projectile(cRect.x+random(0,19)*10,cRect.y,10,10,5,rad(DOWN)));
     }
-
+    //create main cloud that is above snow
+    public void createCloud(){
+        clouds.add(new Projectile(random(0,1200),0,200,50,1,rad(RIGHT)));
+    }
     public void setUp(){
-        projectiles.add(new Projectile(500,500,10,10, rad(350)));
-        projectiles.add(new Projectile(100,100,10,10, rad(280)));
+        projectiles.add(new Projectile(500,500,10,10,1, rad(350)));
+        projectiles.add(new Projectile(100,100,10,10,1,rad(280)));
+        createCloud();
     }
     private void runProj(ArrayList<Projectile> pList) {
         for (int i = 0; i < pList.size(); i++) {
             Projectile proj = pList.get(i);
             if (!proj.remove(pList)) {//projectile is not off screen
-                proj.move();
+                proj.move(true);
                 proj.hit();
             }
             else {//take out of list loop
@@ -123,21 +128,38 @@ public class Battle {
             }
         }
     }
-    public boolean timeMill(int delay){
-        if(System.currentTimeMillis()-memTime>500+random(0,200)){
-            memTime=System.currentTimeMillis();
+    public boolean timeMill(int delay,int index){
+        if(System.currentTimeMillis()-memTime[index]>delay+random(0,200)){
+            memTime[index]=System.currentTimeMillis();
             return true;
         }
         return false;
     }
-    public void move(boolean[] keys){
+    private double distance(Projectile proj,Player player){
+        double dist=0;
+        int pX=player.getRect().x,pY=player.getRect().y,pW=player.getRect().width,pH=player.getRect().height;
+
+        return dist;
+    }
+    public void move(boolean[] keys) {
+
+//        Math.sqrt((double)projectiles.get(0).x+(double)player.getRect().x)
+
 //        runProj(projectiles);
-        if(!snow.isEmpty()) {
+        if (!snow.isEmpty()) {
             runProj(snow);
         }
-        if(timeMill(500+random(0,200))){
-            memTime=System.currentTimeMillis();
-            createSnow(500,0);
+            for (int i = 0; i < clouds.size(); i++) {
+                Projectile cloud = clouds.get(i);
+                if (!cloud.remove(clouds)) {//projectile is not off screen
+                    cloud.move(true);
+                    cloud.hit();
+                    if (timeMill(SNOWGEN + random(0, 200), 0)) {
+                        createSnow(cloud);
+                    }
+                } else {//take out of list loop
+                    i--;
+                }
         }
         player.move(keys);
     }
@@ -147,6 +169,10 @@ public class Battle {
         player.draw(g);
         for(Projectile p:projectiles){
             p.draw(g);
+        }
+        for(Projectile c:clouds){
+            g.setColor(Color.gray);
+            c.draw(g);
         }
         for(Projectile s:snow){
             g.setColor(Color.white);
